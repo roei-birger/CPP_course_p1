8de3f75a65e01cd2316504c34f78a1f9b4f1cfe0
roei-birger <roei1237@gmail.com>
2021-03-08 19:49:22 +0200

    fix the tests

--- Test.cpp
+++ Test.cpp
@@ -42,6 +42,48 @@ TEST_CASE("More digits than needed")
     CHECK_THROWS(snowman(444444444));
 }
 
+TEST_CASE("correct output- hat cases"){
+   CHECK(snowman(11114411) == string("       \n _===_ \n (.,.) \n ( : ) \n ( : ) "));
+    CHECK(snowman(21211121) == string("  ___  \n ..... \n (o,.) \n<(] [)>\n ( : ) "));
+    CHECK(snowman(33224442) == string("   _   \n  /_\\  \n (o_o) \n (   ) \n (\" \") "));
+    CHECK(snowman(43443342) == string("  ___  \n (_*_) \n (-_-) \n/(   )\\\n (\" \") "));
+}
+
+TEST_CASE("correct output- nose cases"){
+    CHECK(snowman(11114411) == string("       \n _===_ \n (.,.) \n ( : ) \n ( : ) "));
+    CHECK(snowman(12124442) == string("       \n _===_ \n (..o) \n (   ) \n (\" \") "));
+    CHECK(snowman(43223343) == string("  ___  \n (_*_) \n (o_o) \n/(   )\\\n (___) "));
+    CHECK(snowman(44323333) == string("  ___  \n (_*_) \n (O o) \n/(> <)\\\n (___) "));
+}
+
+TEST_CASE("correct output- eyes cases"){
+    CHECK(snowman(12124442) == string("       \n _===_ \n (..o) \n (   ) \n (\" \") "));
+    CHECK(snowman(21232224) == string("  ___  \n ..... \n\\(o,O)/\n (] [) \n (   ) "));
+    CHECK(snowman(21211121) == string("  ___  \n ..... \n (o,.) \n<(] [)>\n ( : ) "));
+    CHECK(snowman(43443342) == string("  ___  \n (_*_) \n (-_-) \n/(   )\\\n (\" \") "));
+    CHECK(snowman(44323333) == string("  ___  \n (_*_) \n (O o) \n/(> <)\\\n (___) "));
+}
+
+TEST_CASE("correct output- hands cases"){
+    CHECK(snowman(11111111) == string("       \n _===_ \n (.,.) \n<( : )>\n ( : ) "));
+    CHECK(snowman(21232224) == string("  ___  \n ..... \n\\(o,O)/\n (] [) \n (   ) "));
+    CHECK(snowman(43223344) == string("  ___  \n (_*_) \n (o_o) \n/(   )\\\n (   ) "));
+    CHECK(snowman(11114411) == string("       \n _===_ \n (.,.) \n ( : ) \n ( : ) "));
+}
+
+TEST_CASE("correct output- torso cases"){
+     CHECK(snowman(11114411) == string("       \n _===_ \n (.,.) \n ( : ) \n ( : ) "));
+     CHECK(snowman(21211121) == string("  ___  \n ..... \n (o,.) \n<(] [)>\n ( : ) "));
+     CHECK(snowman(12341231) == string("       \n _===_ \n (O.-)/\n<(> <) \n ( : ) "));
+     CHECK(snowman(33224441) == string("   _   \n  /_\\  \n (o_o) \n (   ) \n ( : ) "));
+}
+
+TEST_CASE("correct output- base cases"){
+    CHECK(snowman(13114411) == string("       \n _===_ \n (._.) \n ( : ) \n ( : ) "));
+    CHECK(snowman(11114422) == string("       \n _===_ \n (.,.) \n (] [) \n (\" \") "));
+    CHECK(snowman(11111113) == string("       \n _===_ \n (.,.) \n<( : )>\n (___) "));
+    CHECK(snowman(43443334) == string("  ___  \n (_*_) \n (-_-) \n/(> <)\\\n (   ) "));   
+}
 
 TEST_CASE("Inserting incorrect literature")
 {
